db76f829842a9aad25a8aa6f3d3a1de9498ae4b4
antonrigner <43144707+antonrigner@users.noreply.github.com>
2018-11-03 12:32:03 +0800

    Grid parameter search

--- Project2/Project2_A_3.py
+++ Project2/Project2_A_3.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 24 11:47:19 2018
+Created on Sat Nov  3 12:28:15 2018
 
 @author: Anton
 """
 
 #
-# Project 2, Part A, Question 1
+# Project 2, Part A, Question 3
 #
 
 #import math
@@ -15,21 +15,22 @@ import numpy as np
 import pylab as plt
 import pickle
 import os
+import itertools
+import timeit
 
-if not os.path.isdir('figuresA1'):
+if not os.path.isdir('figuresA3'):
     print('Creating the figures folder')
-    os.makedirs('figuresA1')
+    os.makedirs('figuresA3')
     
-#TODO: check reasonable acurracy: 40-50%
+#TODO: Grid search suitable parameters, vary seed size. Early stopping, try more iterations, AlexNet...
+# Save figures
     
 NUM_CLASSES = 10 # 10 object classes
 IMG_SIZE = 32 # 32x32 pixels
 NUM_CHANNELS = 3 # RGB channels
-NUM_FILTERS_C1 = 50# Filters in Convolution layer 1
-NUM_FILTERS_C2 = 60 # Filters in Convolution layer 2
 NUM_FCONNECTED = 300 # Fully connected layer
 learning_rate = 0.001 # 0.001
-epochs = 1
+epochs = 5
 batch_size = 128
 
 seed = 10
@@ -54,8 +55,8 @@ def load_data(file):
     labels_[np.arange(labels.shape[0]), labels-1] = 1 # one hot matrix for classes
     
     # Experiment with smaller data size
-    data = data[:500]
-    labels_ = labels_[:500]
+    data = data[:250]
+    labels_ = labels_[:250]
     return data, labels_
 
 def weight_variable(shape):
@@ -63,21 +64,21 @@ def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
 
-#TODO: Initialization
-  
+#TODO: default inititalization tensor flow
+
 def bias_variable(shape):
   """bias_variable generates a bias variable of a given shape."""
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
 
 
-def cnn(images):
+def cnn(images, nbr_ftrMaps1, nbr_ftrMaps2):
 
     images = tf.reshape(images, [-1, IMG_SIZE, IMG_SIZE, NUM_CHANNELS]) # shape = [?, 32, 32, 3]
     
     # Convolutional layer 1, 9x9 Window
-    W_conv1 = weight_variable([9, 9, NUM_CHANNELS, NUM_FILTERS_C1]) # [9, 9, 3, 50]
-    b_conv1 = bias_variable([NUM_FILTERS_C1])
+    W_conv1 = weight_variable([9, 9, NUM_CHANNELS, nbr_ftrMaps1]) # [9, 9, 3, 50]
+    b_conv1 = bias_variable([nbr_ftrMaps1])
     u_conv1 = tf.nn.conv2d(images, W_conv1, strides=[1, 1, 1, 1], padding='VALID') + b_conv1
     conv_1 = tf.nn.relu(u_conv1)
     
@@ -85,8 +86,8 @@ def cnn(images):
     pool_1 = tf.nn.max_pool(conv_1, ksize= [1, 2, 2, 1], strides= [1, 2, 2, 1], padding='VALID', name='pool_1')
     
     # Convolutional layer 2, 5x5 Window
-    W_conv2 = weight_variable([5, 5, NUM_FILTERS_C1, NUM_FILTERS_C2])
-    b_conv2 = bias_variable([NUM_FILTERS_C2])
+    W_conv2 = weight_variable([5, 5, nbr_ftrMaps1, nbr_ftrMaps2])
+    b_conv2 = bias_variable([nbr_ftrMaps2])
     u_conv2 = tf.nn.conv2d(pool_1, W_conv2, strides=[1, 1, 1, 1], padding='VALID') + b_conv2
     conv_2 = tf.nn.relu(u_conv2)
 
@@ -107,18 +108,14 @@ def cnn(images):
     b_softmax = bias_variable([NUM_CLASSES])
     logits = tf.matmul(z_fc, W_softmax) + b_softmax
 
-    return logits, conv_1, conv_2, pool_1, pool_2
+    return logits
 
 
-def main():    
+def runModel(nbr_ftrMaps1, nbr_ftrMaps2):    
     trainX, trainY = load_data('data_batch_1') # load data from file
-    print('trainX shape and trainY shape')
-    print(trainX.shape, trainY.shape)
-    
     testX, testY = load_data('test_batch_trim')
-    print('testX shape and testY shape')
-    print(testX.shape, testY.shape)
-
+    
+    # Scale data
     trainX = (trainX - np.min(trainX, axis = 0))/np.max(trainX, axis = 0) # pixel scaling 0 to 1
     testX = (testX - np.min(testX, axis = 0))/np.max(testX, axis = 0) # pixel scaling 0 to 1
 
@@ -127,7 +124,7 @@ def main():
     y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES])
 
     # Build the layers
-    logits, conv_1, conv_2, pool_1, pool_2 = cnn(x)
+    logits = cnn(x, nbr_ftrMaps1, nbr_ftrMaps2)
     
     # Loss function to minimize
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=logits)
@@ -147,100 +144,84 @@ def main():
         sess.run(tf.global_variables_initializer())
         train_cost = [] 
         test_acc = []
+        epoch_trainingTime = []
+        
         for e in range(epochs):
             np.random.shuffle(idx)
             trainX, trainY = trainX[idx], trainY[idx]
+            
+            beginTimer = timeit.default_timer()
 
             for start, end in zip(range(0, N, batch_size), range(batch_size, N, batch_size)):
                 train_op.run(feed_dict={x: trainX[start:end,:], y_: trainY[start:end,:]})
-
+                
+            stopTimer = timeit.default_timer()
+            diffTime = stopTimer-beginTimer
+            epoch_trainingTime.append(diffTime)
 
             test_acc.append(accuracy.eval(feed_dict={x: testX, y_: testY})) # save accurracy for every epoch   
             loss_ = sess.run(loss, {x: trainX, y_: trainY})
             train_cost.append(loss_)
 
             print('epoch', e, 'entropy', loss_)
-            
-        # After training, plot test images with corresponding convolutional and pooling feature maps
-        # Predictions for the test images are printed
-        nbrTestImages = 2
-        for nbrimg in range(nbrTestImages):
-            ind = np.random.randint(low=0, high=len(testX))
-            X = testX[ind]
-            img = X.reshape(NUM_CHANNELS, IMG_SIZE, IMG_SIZE).transpose(1, 2, 0) # Original image
-            X = X.reshape(1, IMG_SIZE*IMG_SIZE*NUM_CHANNELS) # Transpose
-            conv_1_, conv_2_, pool_1_, pool_2_ = sess.run([conv_1, conv_2, pool_1, pool_2], feed_dict={x: X})
-            
-            # Evaluate class prediction for the test image
-            prediction_ = sess.run(prediction, feed_dict={x: X})
-            # Extract the top 5 most likely classes
-            idxs = np.argsort(prediction_, axis=1)
-            rev = np.fliplr(idxs)
-            np.set_printoptions(precision=4,suppress=True)
-            print('The top 5 predicted classes, left is most probable:\n', rev[:,:5])
-            
-            # Plot original image'
-            print('Original test image with class ', np.argmax(testY[ind]))
-            plt.figure()
-            plt.subplot(1, 1, 1); plt.axis('off'); plt.imshow(img)
-            plt.savefig('./figuresA1/PartA_1_Img' + str(nbrimg) + '.png')
-            plt.show()
-            
-            # Plot convolutional feature maps
-            print('Convolution layer 1 feature maps')
-            plt.figure(figsize=(8,8))
-            for i in range(NUM_FILTERS_C1):
-                plt.subplot(10, 5, i+1); plt.axis('off'); plt.imshow(conv_1_[0, :, :, i])
-            plt.savefig('./figuresA1/PartA_1_Conv1' + str(nbrimg) + '.png')
-            plt.show()
-            
-            # Plot pooling feature maps
-            print('Pool 1 feature maps (MAX pooling)')
-            plt.figure(figsize=(8,8))
-            for i in range(NUM_FILTERS_C1):
-                plt.subplot(10, 5, i+1); plt.axis('off'); plt.imshow(pool_1_[0, :, :, i])
-            plt.savefig('./figuresA1/PartA_1_Pool1' + str(nbrimg) + '.png')
-            plt.show()        
-            
-            # Convolution and pool layer 2
-            print('Convolution layer 2 feature maps')
-            plt.figure(figsize=(8,8))
-            for i in range(NUM_FILTERS_C1):
-                plt.subplot(10, 5, i+1); plt.axis('off'); plt.imshow(conv_2_[0, :, :, i])
-            plt.savefig('./figuresA1/PartA_1_Conv2' + str(nbrimg) + '.png')
-            plt.show()
-            
-            print('Pool 2 feature maps (MAX pooling)')
-            plt.figure(figsize=(8,8))
-            for i in range(NUM_FILTERS_C1):
-                plt.subplot(10, 5, i+1); plt.axis('off'); plt.imshow(pool_2_[0, :, :, i])
-            plt.savefig('./figuresA1/PartA_1_Pool2' + str(nbrimg) + '.png')
-            plt.show()      
+                
+        modelData = np.zeros((3,epochs))
+        modelData[0, :] = train_cost
+        modelData[1, :] = test_acc
+        modelData[2, :] = epoch_trainingTime
+    return modelData
 
+def main():
+    # Initialize for plotting purposes
+    train_cost = []
+    test_acc = []
+    avgtime = []
+    legend = []
+    fig1 = plt.figure(2, figsize=(10,5))
+    ax1 = fig1.add_subplot(111)
+    ax1.set_title('Training Cost (Cross entropy)')
+    ax1.set_xlabel(str(epochs) + ' iterations/epochs')
+    ax1.set_ylabel('Cross entropy')
+    
+    fig2 = plt.figure(3, figsize=(10,5))
+    ax2 = fig2.add_subplot(111)
+    ax2.set_title('Top 1 Test Accurracy')
+    ax2.set_xlabel(str(epochs) + ' iterations/epochs')
+    ax2.set_ylabel('Test accuracy')
+    
+#TODO: Evaluated broad grid -> close in on goal, evaluate again -> report top 5 in plot, rest in grid/matrix in the report
+    # Run models, Grid search (parameter sweep) to evaluate models 
+    c1Fmaps = np.array([5,10])# nbr of filters (feature maps)
+    c2Fmaps = np.array([5,10])
+    for paramSet in itertools.product(c1Fmaps,c2Fmaps): 
+        print(paramSet[0], paramSet[1])
+        modelData = runModel(paramSet[0], paramSet[1])
+        train_cost = modelData[0]
+        test_acc = modelData[1]
+        print(np.mean(modelData[2]))
+        avgtime.append(np.mean(modelData[2])) # average out how much time to train ONE epoch
+        
+        ax1.plot(range(epochs), train_cost)
+        ax2.plot(range(epochs), test_acc)
+        # Store pairs for plot legend
+        stringPair = str(paramSet[0]) + ', ' + str(paramSet[1])
+        legend.append(stringPair)
+        
+    fig1.legend(legend)
+#    fig1.savefig('./figuresA2/PartA_2_TrainError.png')
+    fig1.show()
+    
+    fig2.legend(legend)
+#    fig2.savefig('./figuresA2/PartA_2_TestAcc.png')
+    fig2.show()
+    
     plt.figure(1)
-    plt.title('Training Cost (Cross entropy)')
-    plt.plot(range(epochs), train_cost)
-    plt.xlabel(str(epochs) + ' iterations')
-    plt.ylabel('Cross entropy')
-    plt.savefig('./figuresA1/PartA_1_TrainCost.png')
+    plt.title('Average time for one epoch for different nbr of feature maps')
+#    plt.xticks(range(25))
+    plt.scatter(legend, avgtime)
+    plt.xlabel('Sets of feature maps')
+    plt.ylabel('Average epoch training time')
+    plt.savefig('./figuresA2/PartA_2_TrainTime.png')
     plt.show()
-    
-    plt.figure(2)
-    plt.title('Top 1 Test Accurracy')
-    plt.plot(range(epochs), test_acc)
-    plt.xlabel(str(epochs) + ' iterations')
-    plt.ylabel('Test accuracy')
-    plt.savefig('./figuresA1/PartA_1_TestAcc.png')
-    plt.show()
-
-    #TODO: Add top 5 acc
-    
-#    plt.figure(3)
-#    plt.title('Top 5 Test Accurracy for 4 layer network')
-#    plt.plot(range(epochs), test_accTop5)
-#    plt.xlabel(str(epochs) + ' iterations')
-#    plt.ylabel('Test accuracy')
-#    plt.savefig('./figuresA1/PartA_1_Top5TestAcc.png')
-#    plt.show()
 if __name__ == '__main__':
   main()
